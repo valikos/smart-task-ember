7fc6995090d77ffe6a6d27f2ea08bdc6309e968b
Valentyn Ostakh <valikos.ost@gmail.com>
2016-09-26 20:15:14 +0300

    Rollback env configs

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,8 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+    ENV.apiHost = 'http://smart_task_api.dev';
+    // ENV.apiHost = 'http://localhost:3000';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
